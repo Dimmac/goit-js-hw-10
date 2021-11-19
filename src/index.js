0768c5f66a6563df5227af515a3a14e42f1b3efd
Dimmac <84150134+Dimmac@users.noreply.github.com>
2021-11-19 02:33:33 +0200

    markup

--- src/index.js
+++ src/index.js
@@ -1,31 +1,48 @@
 import './css/styles.css';
-import lodash from 'lodash.debounce';
+// import lodash from 'lodash.debounce';
 import notiflix from 'notiflix';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
-// const BASE_URL = 'https://restcountries.com/v3.1/name/{peru}';
 
 const refs = {
   searchBox: document.querySelector('#search-box'),
+  countryInfo: document.querySelector('.country-info'),
 };
 
-refs.searchBox.addEventListener('input', onSearch);
+refs.searchBox.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
 function onSearch() {
   const searchQuery = refs.searchBox.value;
   console.log(searchQuery);
-
-  fetchCountries(searchQuery)
-    .then(console.log(name))
-    .catch(error => {
-      console.log(error);
-    });
+  const options = {};
+  fetchCountries(searchQuery, options)
+    .then(name => renderCountryList(name))
+    .catch(error => console.log(error));
 }
 
-function fetchCountries(name) {
-  return fetch(`https://restcountries.com/v3.1/name/${name}`).then(response => {
-    return response.json();
-  });
-}
+// function onFetchError() {
+//   alert('we have problems');
+// }
+// function onFetchDone() {
+//   alert('THEN IS DONE!');
+// }
 //uganda
+
+function renderCountryList(name) {
+  if (name.length > 10) {
+    return notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+  }
+  const markup = name
+    .map(user => {
+      return `<li>
+          <p><b></b>${user.flags.svg}</p>
+          <p><b></b>${user.name.official}</p>
+        </li>`;
+    })
+    .join('');
+  refs.countryInfo.innerHTML = markup;
+  console.log(name.length);
+}
